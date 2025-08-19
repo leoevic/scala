ffe54356867a0e656443204612f6df3921587ca5
Leonardo Evic <evicleonardo@gmail.com>
2025-08-19 10:55:11 +0200

    Add exercise 4 - squares of a set

--- src/main/scala/ch/bfo/mystuff/HelloWorld.scala
+++ src/main/scala/ch/bfo/mystuff/HelloWorld.scala
@@ -5,6 +5,7 @@ import scala.io.StdIn.readLine
 @main def helloWorld(): Unit =
   println("Hello, World!")
 
+/* Exercise 1: Temperatures on a single line */
 @main def main1(): Unit =
   println("Enter several temperatures on a single line ")
   val numbers = readLine().split(" ").map(_.toInt)
@@ -14,6 +15,7 @@ import scala.io.StdIn.readLine
 def countBelowZero(numbers: Array[Int]): Int =
   numbers.count(i => i < 0)
 
+/* Exercise 2: Get characters of a word */
 @main def main2(): Unit =
   println("Geben Sie ein Wort ein")
   val word = readLine().toLowerCase().toCharArray
@@ -23,3 +25,23 @@ def countBelowZero(numbers: Array[Int]): Int =
 
 def getLetters(word: Array[Char]): Set[Char] =
   word.filter(i => i.toString().matches("[A-Za-z]")).toSet
+
+/* Exercise 3: Reverse order of words and make them UPPERCASE */
+@main def main3(): Unit =
+  println("Geben Sie einen Satz ein:")
+  val sentence = readLine()
+  val newSentence = revertAndUpperCase(sentence)
+  println(newSentence.toString)
+
+def revertAndUpperCase(sentence: String): String =
+  sentence.split(" ").map(_.toUpperCase).reverse.mkString(" ")
+
+
+/* Exercise 4: Squares of a set */
+@main def main4(): Unit =
+  val numbersSet = Set(1, 2, 3, 4, 5, 6, 7, 8, 9)
+  val newSet = squaredSet(numbersSet)
+  newSet.foreach(println)
+
+def squaredSet(numbers: Set[Int]): Set[Int] =
+  numbers.map(i => i * i)
